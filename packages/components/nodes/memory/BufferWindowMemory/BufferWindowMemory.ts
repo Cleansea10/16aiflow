2f11fe67bdc748ad434f91816e49d86446cb2983
f-amine <saddeksimo10@gmail.com>
2023-12-22 12:40:55 -0800

    Default commit message - Wed Jan 22 04:26:27 PM +01 2025

--- packages/components/nodes/memory/BufferWindowMemory/BufferWindowMemory.ts
+++ packages/components/nodes/memory/BufferWindowMemory/BufferWindowMemory.ts
@@ -159,3 +159,4 @@ class BufferWindowMemoryExtended extends FlowiseWindowMemory implements MemoryMe
 }
 
 module.exports = { nodeClass: BufferWindowMemory_Memory }
+// Default commit message - Wed Jan 22 04:26:27 PM +01 2025
