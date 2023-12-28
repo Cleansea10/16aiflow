7c99ce9216298acfaf352ec5e24ffa6bf8e424d7
f-amine <saddeksimo10@gmail.com>
2023-12-27 21:52:26 -0800

    Default commit message - Wed Jan 22 04:26:28 PM +01 2025

--- packages/server/src/AppConfig.ts
+++ packages/server/src/AppConfig.ts
@@ -5,3 +5,4 @@ export const appConfig = {
     showCommunityNodes: process.env.SHOW_COMMUNITY_NODES ? process.env.SHOW_COMMUNITY_NODES.toLowerCase() === 'true' : false
     // todo: add more config options here like database, log, storage, credential and allow modification from UI
 }
+// Default commit message - Wed Jan 22 04:26:28 PM +01 2025
