a8ab12941e08e45247a68beb2a30d2fbcf790d0e
f-amine <saddeksimo10@gmail.com>
2023-12-30 17:04:23 -0800

    Default commit message - Wed Jan 22 04:26:29 PM +01 2025

--- packages/server/src/utils/addChatMesage.ts
+++ packages/server/src/utils/addChatMesage.ts
@@ -17,3 +17,4 @@ export const utilAddChatMessage = async (chatMessage: Partial<IChatMessage>): Pr
     const dbResponse = await appServer.AppDataSource.getRepository(ChatMessage).save(chatmessage)
     return dbResponse
 }
+// Default commit message - Wed Jan 22 04:26:29 PM +01 2025
