878efdeb7432c1c583bc53d3a115173068bd54ca
f-amine <saddeksimo10@gmail.com>
2024-04-23 21:13:15 -0700

    feat: Implement backup system

--- packages/server/src/database/migrations/mysql/1693999261583-ModifyCredential.ts
+++ packages/server/src/database/migrations/mysql/1693999261583-ModifyCredential.ts
@@ -9,3 +9,4 @@ export class ModifyCredential1693999261583 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`credential\` MODIFY \`encryptedData\` VARCHAR NOT NULL;`)
     }
 }
+// Modified on Wed Jan 22 04:34:22 PM +01 2025
