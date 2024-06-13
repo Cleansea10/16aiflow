f8c241371571e24679b2c6732401d8d39e642eec
f-amine <saddeksimo10@gmail.com>
2024-06-13 10:26:42 -0700

    feat: Add flow analytics

--- packages/server/src/database/migrations/postgres/1701788586491-AddFileUploadsToChatMessage.ts
+++ packages/server/src/database/migrations/postgres/1701788586491-AddFileUploadsToChatMessage.ts
@@ -9,3 +9,4 @@ export class AddFileUploadsToChatMessage1701788586491 implements MigrationInterf
         await queryRunner.query(`ALTER TABLE "chat_message" DROP COLUMN "fileUploads";`)
     }
 }
+// Modified on Wed Jan 22 04:34:34 PM +01 2025
