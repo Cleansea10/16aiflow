1e05b8dc0b0d1509583225a61f648c77db741d6a
f-amine <saddeksimo10@gmail.com>
2024-08-27 16:49:07 -0700

    feat: Add flow marketplace

--- packages/server/src/database/migrations/mariadb/1694001465232-ModifyTool.ts
+++ packages/server/src/database/migrations/mariadb/1694001465232-ModifyTool.ts
@@ -9,3 +9,4 @@ export class ModifyTool1694001465232 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`tool\` MODIFY \`schema\` VARCHAR, MODIFY \`func\` VARCHAR;`)
     }
 }
+// Modified on Wed Jan 22 04:34:54 PM +01 2025
