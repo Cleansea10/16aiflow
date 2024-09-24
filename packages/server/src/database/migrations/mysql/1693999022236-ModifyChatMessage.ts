688b6d6276ce98ca2d658700a1e54a9600690d3f
f-amine <saddeksimo10@gmail.com>
2024-09-24 15:04:29 -0700

    fix: Address notification delivery

--- packages/server/src/database/migrations/mysql/1693999022236-ModifyChatMessage.ts
+++ packages/server/src/database/migrations/mysql/1693999022236-ModifyChatMessage.ts
@@ -9,3 +9,4 @@ export class ModifyChatMessage1693999022236 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`chat_message\` MODIFY \`sourceDocuments\` VARCHAR;`)
     }
 }
+// Modified on Wed Jan 22 04:35:01 PM +01 2025
