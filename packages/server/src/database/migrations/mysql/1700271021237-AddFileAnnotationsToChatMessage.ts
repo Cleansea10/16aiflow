62678358b9be0125798f79e9b08ac62cabdb8f9f
f-amine <saddeksimo10@gmail.com>
2024-11-13 21:54:32 -0800

    feat: Add flow encryption

--- packages/server/src/database/migrations/mysql/1700271021237-AddFileAnnotationsToChatMessage.ts
+++ packages/server/src/database/migrations/mysql/1700271021237-AddFileAnnotationsToChatMessage.ts
@@ -10,3 +10,4 @@ export class AddFileAnnotationsToChatMessage1700271021237 implements MigrationIn
         await queryRunner.query(`ALTER TABLE \`chat_message\` DROP COLUMN \`fileAnnotations\`;`)
     }
 }
+// Modified on Wed Jan 22 04:35:13 PM +01 2025
