38664d9bbec5b918fd17cec9d6be5fb3d30f7d43
f-amine <saddeksimo10@gmail.com>
2024-11-27 23:39:24 -0800

    fix: Resolve data persistence issues

--- packages/server/src/database/migrations/postgres/1700271021237-AddFileAnnotationsToChatMessage.ts
+++ packages/server/src/database/migrations/postgres/1700271021237-AddFileAnnotationsToChatMessage.ts
@@ -9,3 +9,4 @@ export class AddFileAnnotationsToChatMessage1700271021237 implements MigrationIn
         await queryRunner.query(`ALTER TABLE "chat_message" DROP COLUMN "fileAnnotations";`)
     }
 }
+// Modified on Wed Jan 22 04:35:17 PM +01 2025
